ff0277f72439257e26fae675c9aa760cb82e4293
Moh <mohamed@somehwere.com>
2022-05-10 14:00:57 +0100

    frontend starting point

---
+++ src/Nav.js
@@ -0,0 +1,41 @@
+import './App.css';
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.css'
+import { Button, Nav, Navbar, Container, Form, FormControl } from 'react-bootstrap';
+
+
+
+function header() {
+  return (
+
+    <div>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">QA Cinema</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#listings">Listings</Nav.Link>
+              <Nav.Link href="#newReleases">New Releases</Nav.Link>
+              <Nav.Link href="#screens">Screens</Nav.Link>
+              <Nav.Link href="#bookTickets">Books Tickets</Nav.Link>
+            </Nav>
+            <Form className="d-flex">
+              <FormControl
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-light dark">Search</Button>
+            </Form>
+
+
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+}
+
+export default header;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Nav from './Nav';
+import Footer from './Footer';
+import Body from './Body'
 import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+
+      <Nav />
+      <Body />
+      <Footer />
+
   </React.StrictMode>
 );
 
