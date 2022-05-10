922508aa3ba4a45c76a8d6a7febe31bdf4666a29
Jonah Atkinson <theJAtkinson@gmail.com>
2022-05-10 16:33:07 +0100

    Styled openingTimes

--- src/OpeningTimes.js
+++ src/OpeningTimes.js
@@ -1,57 +1,61 @@
 import React from 'react'
-import { Table } from 'react-bootstrap';
+import { Table, Container } from 'react-bootstrap';
 import './App.css';
 import './index.css';
 
 
 const OpeningTimes = () =>
-    <div >
-        <h1>OPENING TIMES</h1>
-        <Table striped bordered hover variant="dark">
-            <thead>
-                <tr>
-                    <th>Days</th>
-                    <th>Time</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <th>Monday</th>
-                    <th>11:00-23:00</th>
+    <div>
+        <Container>
+        <br/>
+            <h1 style={{color: "white"}}><b>OPENING TIMES</b></h1>
+            <br/>
+            <Table bordered hover variant="dark" style={{ "max-width": "40%" }}>
+                <thead>
+                    <tr>
+                        <th>Days</th>
+                        <th>Time</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th>Monday</th>
+                        <th>11:00-23:00</th>
 
-                </tr>
-                <tr>
-                    <th>Tuesday</th>
-                    <th>11:00-23:00</th>
+                    </tr>
+                    <tr>
+                        <th>Tuesday</th>
+                        <th>11:00-23:00</th>
 
-                </tr>
-                <tr>
-                    <th>Wednesday</th>
-                    <th>11:00-23:00</th>
+                    </tr>
+                    <tr>
+                        <th>Wednesday</th>
+                        <th>11:00-23:00</th>
 
-                </tr>
-                <tr>
-                    <th>Thursday</th>
-                    <th>11:00-23:00</th>
+                    </tr>
+                    <tr>
+                        <th>Thursday</th>
+                        <th>11:00-23:00</th>
 
-                </tr>
-                <tr>
-                    <th>Friday</th>
-                    <th>11:00-23:00</th>
+                    </tr>
+                    <tr>
+                        <th>Friday</th>
+                        <th>11:00-23:00</th>
 
-                </tr>
-                <tr>
-                    <th>Saturday</th>
-                    <th>11:00-23:00</th>
+                    </tr>
+                    <tr>
+                        <th>Saturday</th>
+                        <th>11:00-23:00</th>
 
-                </tr>
-                <tr>
-                    <th>Sunday</th>
-                    <th>11:00-23:00</th>
+                    </tr>
+                    <tr>
+                        <th>Sunday</th>
+                        <th>11:00-23:00</th>
 
-                </tr>
-            </tbody>
-        </Table>
+                    </tr>
+                </tbody>
+            </Table>
+        </Container>
     </div>;
 
 export default OpeningTimes;
