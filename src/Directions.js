f22e72000757b68c818a16eb2cab428fc097ef25
Jonah Atkinson <theJAtkinson@gmail.com>
2022-05-11 15:23:13 +0100

    completed directions

--- src/Directions.js
+++ src/Directions.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Image } from 'react-bootstrap';
+import imgLocation from './images/Location.png';
+import imgCinema from './images/Cinema.jpg';
 
 const Directions = () =>
     <div>
@@ -9,18 +11,41 @@ const Directions = () =>
             <br />
 
             <Row>
-                <Col>
-                    Location:
+                <Col xs={3}>
+
+                    <h3><b>Location</b></h3>
+                    1st Floor <br />
+                    International House <br />
+                    1 St Katharines Way <br />
+                    London <br />
+                    E1W 1UN
+
+                    <br/>
+                    <br/>
+                    <br/>
+
+                    <h3><b>Transport</b></h3>
+                Nearest tube station: <br/>
+                Tower Hill Station
+
                 </Col>
-                <Col>
-                    1st Floor <br/>
-                    International House <br/>
-                    1 St Katharines Way <br/>
-                    London <br/>
-                    E1W 1UN 
+                <Col xs={9}>
+                    <Image fluid="false" src={imgLocation} width="60%" />
                 </Col>
             </Row>
+            <br/>
+            <Row>
+                <Col xs={3}>
+                
+                </Col>
+                <Col xs={9}>
+                    <Image fluid="false" src={imgCinema} width="60%" />
+                </Col>
+
+            </Row>
+
         </Container>
+
     </div>
 
 export default Directions;
